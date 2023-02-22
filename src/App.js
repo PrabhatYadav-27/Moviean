a0ad30d3c27a5f9b9d2a6755af961878f1f49d7e
PrabhatYadav-27 <prabhaty2727@gmail.com>
2023-02-22 15:42:01 +0530

    Added Routing in our website

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import NavBar from './Components/NavBar';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import Pagination from './Components/Pagination';
+import { BrowserRouter,Routes,Route }  from 'react-router-dom'
+import Favourites from './Components/Favourites';
+import PageNotFound from './Components/PageNotFound';
 function App() {
   return (
     <>
-    {/* <h1>Hello React</h1> */}
+    <BrowserRouter>
     <NavBar></NavBar>
-    <Banner></Banner>
-    <Movies></Movies>
-    <Pagination></Pagination>
+    {/* <h1>Hello React</h1> */}
+    <Routes>
+      <Route path ="/" element = {
+        <>
+         <Banner></Banner>
+        <Movies></Movies>
+        <Pagination></Pagination>
+        </>
+      }></Route>
+      <Route path="/fav" element = {
+        <Favourites></Favourites>
+      }></Route>
+      <Route path="*" element={
+        <PageNotFound></PageNotFound>
+      }></Route>
+    </Routes>
+    
+   
+    </BrowserRouter>
+    
     
     </>
   );
---
+++ src/Components/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div>You Are On Wrong Page_404</div>
+  )
+}
+
+export default PageNotFound
