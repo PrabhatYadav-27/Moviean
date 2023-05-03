de506599f03b96b2b3309dcbfa540d3de58d06b7
PrabhatYadav-27 <prabhaty2727@gmail.com>
2023-05-03 19:42:42 +0530

    done  with fav

--- src/Components/Favourites.jsx
+++ src/Components/Favourites.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import Pagination from "./Pagination"
-
+import React, { useEffect,useState } from 'react';
+import Pagination from "./Pagination";
 
 
 function Favourites() {
-  let[genres,setGenres] = useState([]);
-
+  let [genres, setGenres] = useState([]);
   let genreids = {
     28: 'Action',
     12: 'Adventure',
@@ -18,7 +16,6 @@ function Favourites() {
     10752: 'War',
     37: 'Western'
   }
-
   let movies = [
     {
       "adult": false,
@@ -114,111 +111,100 @@ function Favourites() {
 
 
 
+
   return (
     <>
-    <div className="mt-6 flex space-x-2 justify-center">
-    {genres.map((genre => {
+      <div className="mt-6 flex space-x-2 justify-center">
+        
+        {genres.map((genre => {
           return (
             <button
               className='py-1 px-2 bg-gray-400 rounded-lg font-bold text-lg text-white hover:bg-blue-400'
             > {genre}</button>
           )
         }))}
-    </div>
 
-    <div className='mt-4 flex justify-center space-x-2'>
-    <input type="text"  placeholder='search '
-    className='border-2  py-1 px-2 text-center'/>
+      </div>
+      <div className="mt-4 flex justify-center space-x-2
+      ">
+        <input type="text" placeholder='search'
+          className=" border-2 py-1 px-2 text-center"
+        />
+        <input type="number" className="border-2 py-1 px-2 text-center"
+          value={1}
+        />
+      </div>
+      <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+        <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+          <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                <div className='flex'>
+                  <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png" class="mr-2 cursor-pointer"></img>
+                  <div>Rating</div>
+                  <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-down-arrows-those-icons-lineal-those-icons-4.png" class="ml-2 mr-2"></img>
+                </div>
 
+              </th>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900 ">
+                <div className='flex'>
+                  <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png" class="mr-2 cursor-pointer"></img>
+                  <div>Popularity</div>
+                  <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-down-arrows-those-icons-lineal-those-icons-4.png" class="ml-2 mr-2"></img>
+                </div>
+              </th>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900 ">Genre</th>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900">Remove</th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+            {movies.map((movie) => {
+              { console.log(movie) };
+              return <tr class="hover:bg-gray-50" key={movie.id}>
+                <th class="flex items-center px-6 py-4 font-normal text-gray-900 space-x-2">
+                  <img
+                    class="h-[6rem]  w-[10rem] object-fit"
+                    src={`https://image.tmdb.org/t/p/original/t/p/original/${movie.poster_path}`}
 
-    <input type="number"
-    className='border-2  py-1 px-2 text-center'
-    value={1} />
-    </div>
+                    alt=""
+                  />
+                  <div class="font-medium text-gray-700  text-sm">{movie.title || movie.name}</div>
 
-    <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
-  <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
-    <thead class="bg-gray-50">
-      <tr>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">NAME</th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">
-          <div className='flex'>
-          <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png" class="mr-2 cursor-pointer"></img>
-          <div>RATING</div>
-          <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png" class="mr-2 cursor-pointer"></img>
-          </div>
-        
-          </th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900"><div className='flex'>
-          <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png" class="mr-2 cursor-pointer"></img>
-          <div>POPULARITY</div>
-          <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png" class="mr-2 cursor-pointer"></img>
-          </div></th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">GENRE</th>
-        <th scope="col" class="px-6 py-4 font-medium text-gray-900">REMOVE</th>
-      </tr>
-    </thead>
-    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-      {movies.map ((movie) =>{
-        {console.log(movie)}
-        return <tr class="hover:bg-gray-50 " key={movie.id}>
-        <th class="flex items-center px-6 py-4 font-normal text-gray-900 space-x-2">
-          
-            <img
-              class="h-[5rem] w-[6rem] object-fit"
-              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-              
-              alt=""
-            />
-    
-            <div class="font-medium text-gray-700 text-sm">{movie.title || movie.name}</div>
-            
-          
-        </th>
-        <td class="px-6 pl-12 py-4">
-          <span
-            class="inline-flex items-center gap-1 rounded-full bg-gray-50 px-2 py-1 text-xs font-semibold text-gray-600"
-          >
-            
-            {movie.vote_average.toFixed(2)}
-          </span>
-        </td>
-        <td class="px-6  pl-12 py-4">{movie.popularity.toFixed(2)}</td>
-        <td class="px-6 py-4">
-          <div class="flex gap-2">
-            <span
-              class="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600"
-            >
-              {genreids[movie.genre_ids[0]]}
-            </span>
-            
-          </div>
-        </td>
-        <td class="px-6 py-4">
-        <span
-              class="inline-flex items-center gap-1 rounded-full  px-2 py-1 text-xs font-semibold text-red-600"
-            >
-              Delete
-            </span>
-        </td>
-      </tr>
+                </th>
+                <td class="px-6 pl-12 py-4">
+                  {movie.vote_average.toFixed(2)}
+                </td>
+                <td class="px-6 py-4 pl-12">{movie.popularity.toFixed(2)}</td>
+                <td class="px-6 py-4">
+                  <div class="flex gap-2">
+                    <span
+                      class="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600"
+                    >
+                      {genreids[movie.genre_ids[0]]}
+                    </span>
 
-      })}   
-    
-      
-      
-    </tbody>
-  </table>
-</div>
+                  </div>
+                </td>
+                <td class="px-6 py-4">
+                  <span
+                    class="inline-flex items-center gap-1 rounded-full px-2 py-1 text-xs font-semibold text-red-600"
+                  >
+                    Delete
+                  </span>
+                </td>
+              </tr>
+            })}
 
 
-    {/* pagination part */}
-    <Pagination></Pagination>
-    
+
+
+          </tbody>
+        </table>
+      </div>
+      <Pagination></Pagination>
     </>
   )
 }
 
-
-
-export default Favourites
+export default Favourites;
